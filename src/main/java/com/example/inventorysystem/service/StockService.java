f5bdbbb7201be06b38b28d89468680d922943a5b
sangjin <insangjin6439@gmail.com>
2025-05-29 15:22:37 +0900

    Synchronized를 통한 동시성 제어

--- src/main/java/com/example/inventorysystem/service/StockService.java
+++ src/main/java/com/example/inventorysystem/service/StockService.java
@@ -15,8 +15,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity){
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity){
         // Stock 조회
         // 재고를 감소시킨 뒤
         // 갱신된 값을 저장
