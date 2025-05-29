78aaac68a4871908c7258f63696bf99194e88f43
sangjin <insangjin6439@gmail.com>
2025-05-29 14:34:38 +0900

    멀티스레드 환경에서 동시에 요청을 보내 RaceCondition이 발생하는 테스트 코드 작성

--- src/test/java/com/example/inventorysystem/service/StockServiceTest.java
+++ src/test/java/com/example/inventorysystem/service/StockServiceTest.java
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -37,6 +41,29 @@ class StockServiceTest {
         //100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        assertEquals(99,stock.getQuantity());
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 멀티 스레드 환경 구현
+        CountDownLatch latch = new CountDownLatch(threadCount); //총 100개 요청
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try { // 예외가 발생할 수 있음
+                    StockService.decrease(1L, 1L);
+                } finally { // 예외 발생해도 무조건 실행
+                    latch.countDown(); // 이 스레드 작업 끝 -> 카운트 감소
+                }
+            });
+        }
+
+        latch.await(); // 100개 모두 끝날 때까지 기다림
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - ( 1 * 100 ) = 0
+        assertEquals(0,stock.getQuantity());
     }
 }
